f8c806bf23c70f02f33e2dbdfe2a643650dcaba7
William <hello@bigwilliam.com>
2014-10-16 18:42:55 -0700

    Added accounts and added template to the header

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,25 +1,26 @@
-var postsData = [
-	{
-		title: 'Introducing Telescope',
-		author: 'Sacha Greif',
-		url: 'http://sachagreif.com/introducing-telescope/'
-	},
-	{
-		title: 'Meteor',
-		author: 'Tom Coleman',
-		url:    'http://meteor.com'
-	},
-	{
-		title: 'The Meteor Book',
-		author: 'Tom Coleman',
-		url: 'http://themeteorbook.com'
-	},	
-	{
-		title: 'More stuff',
-		author: 'William Cobb',
-		url: 'http://bigwilliam.com'
-	}	
-];
+// Deprecated values. Using the db now.
+// var postsData = [
+// 	{
+// 		title: 'Introducing Telescope',
+// 		author: 'Sacha Greif',
+// 		url: 'http://sachagreif.com/introducing-telescope/'
+// 	},
+// 	{
+// 		title: 'Meteor',
+// 		author: 'Tom Coleman',
+// 		url:    'http://meteor.com'
+// 	},
+// 	{
+// 		title: 'The Meteor Book',
+// 		author: 'Tom Coleman',
+// 		url: 'http://themeteorbook.com'
+// 	},	
+// 	{
+// 		title: 'More stuff',
+// 		author: 'William Cobb',
+// 		url: 'http://bigwilliam.com'
+// 	}	
+// ];
 
 Template.postsList.helpers({
 	// posts: postsData
